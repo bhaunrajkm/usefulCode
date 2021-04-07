c0313b09fd3becde26ee4ee0d1add05b4201c2a2
Bhanuraj KM <bhanuraj.km@gmail.com>
2021-04-07 16:13:40 +0530

    Update SwaggerConfiguration.java

--- useful-code/src/main/java/com/bhanu/usefulcode/swagger/SwaggerConfiguration.java
+++ useful-code/src/main/java/com/bhanu/usefulcode/swagger/SwaggerConfiguration.java
@@ -1,5 +1,10 @@
 package com.bhanu.usefulcode.swagger;
 
 public class SwaggerConfiguration {
-
+  @Bean
+  @ConditionalOnProperty(name = "runtime.context.environment", havingValue = "PROD")
+  public UiConfiguration submitMethodsDisabledForProd() {
+    // for Prod env, we will not be able to trigger the functionality from Swagger.
+    return UiConfigurationBuilder.builder().supportedSubmitMethods(new String[] {}).build();
+  }
 }
