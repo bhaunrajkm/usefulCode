f646e55fa6633f3871c139342d458301d9a1942e
Bhanuraj KM <b0k02nq@homeoffice.wal-mart.com>
2021-04-09 09:36:07 +0530

    adding static code analysis

---
+++ useful-code/src/main/java/com/bhanu/usefulcode/staticCodeAnalysis/GenericConfig.java
@@ -0,0 +1,11 @@
+package com.bhanu.usefulcode.staticCodeAnalysis;
+
+import java.util.List;
+
+public class GenericConfig {
+
+    @SuppressWarnings("rawtypes")
+    private List<String> extensions() {
+        return List.of();
+    }
+}
